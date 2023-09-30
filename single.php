e354b08a4782a78fa7376feb0d0bd538ac9e8ab1
Renato Marques <renatodelimamarques@gmail.com>
2023-09-30 17:32:17 -0300

    Menores

--- single.php
+++ single.php
@@ -12,30 +12,38 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
+   <div class="container py-12">
+      <div class="flex gap-6"><!-- flex-grow -->
+         <div class="flex-1 flex-col flex gap-8">
+            <?php
+            $i = 0;
+            if (have_posts()) :
 
-   <?php
-   while (have_posts()) :
-      the_post();
+               while (have_posts()) :
+                  the_post();
 
-      get_template_part('template-parts/content', get_post_type());
+                  get_template_part('template-parts/content', get_post_type(), [
+                     'index' => $i
+                  ]);
+                  $i++;
+               endwhile;
 
-      the_post_navigation(
-         array(
-            'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'portaldoenvelhecimento') . '</span> <span class="nav-title">%title</span>',
-            'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'portaldoenvelhecimento') . '</span> <span class="nav-title">%title</span>',
-         )
-      );
+            else :
 
-      // If comments are open or we have at least one comment, load up the comment template.
-      if (comments_open() || get_comments_number()) :
-         comments_template();
-      endif;
+               get_template_part('template-parts/content', 'none');
 
-   endwhile; // End of the loop.
-   ?>
+            endif;
+            ?>
+         </div>
+         <?php
+
+         get_template_part('template-parts/content', 'sidebar');
+
+         ?>
+      </div>
+   </div>
 
 </main><!-- #main -->
-
 <?php
-get_sidebar();
+
 get_footer();
---
+++ template-parts/content-post.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Template part for post single
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package portaldoenvelhecimento
+ */
+
+$index = $args['index'] ?? false;
+
+$content_class = $index === 0 ? 'flex-1' : 'flex-1';
+$title_class = $index === 0 ? 'text-4xl font-medium' : 'text-2xl font-medium';
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+   <div class="flex gap-10 flex-col items-center">
+      <div class="portaldoenvelhecimento-post-thumbnail w-full">
+         <?php portaldoenvelhecimento_post_thumbnail(null, 'w-full max-full rounded-lg object-cover lg:aspect-16/6 aspect-video'); ?>
+      </div>
+      <div class="<?php echo $content_class ?> flex flex-col gap-2">
+         <div class="flex gap-2.5">
+            <?php
+            $tags = get_the_tags();
+
+            if ($tags)
+            {
+               $contador = 0;
+               foreach ($tags as $tag)
+               {
+                  $contador++;
+                  if ($contador <= 3)
+                  {
+                     echo '<a class="py-1 px-2 bg-blue-500 rounded-md text-white text-xs hover:text-white" href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a>';
+                  }
+               }
+            }
+            ?>
+         </div>
+         <header class="entry-header">
+            <?php
+            if (is_singular()) :
+               the_title('<h1 class="' . $title_class . '">', '</h1>');
+            else :
+               the_title('<h2 class="' . $title_class . '"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+            endif;
+
+            if ('post' === get_post_type()) :
+            ?>
+               <div class="entry-meta">
+                  <?php
+                  portaldoenvelhecimento_posted_on();
+                  // portaldoenvelhecimento_posted_by();
+                  ?>
+               </div><!-- .entry-meta -->
+            <?php endif; ?>
+         </header><!-- .entry-header -->
+
+
+         <div class="entry-content text-gray-600 text-base">
+            <?php
+            echo show_excerpt(-1, '');
+            ?>
+            <div class="mt-6">
+               <?php the_content() ?>
+            </div>
+         </div><!-- .entry-content -->
+      </div>
+   </div>
+   <div class="w-full">
+      <?php
+      $author_id = get_the_author_meta('ID');
+      $author_name = get_the_author_meta('display_name', $author_id);
+      $author_bio = get_the_author_meta('description', $author_id);
+      $author_avatar = get_avatar_url($author_id, array('size' => 150));
+
+      $post_count = count_user_posts($author_id);
+      ?>
+
+      <div class="bg-gray-100 p-4 mb-4 mt-6 rounded-lg">
+         <div class="flex mb-4 gap-4">
+            <div class="flex-shrink-0">
+               <img class="w-[5rem] h-[5rem] rounded-full" src="<?php echo $author_avatar; ?>" alt="Avatar do Autor">
+            </div>
+            <div class="flex flex-col gap-4">
+               <h3 class="text-xl font-bold text-gray-800 mb-0"><?php echo $author_name; ?></h3>
+               <p class="text-base text-gray-500 mb-0"><?php echo $author_bio; ?></p>
+               <p class="text-gray-400 text-xs uppercase mb-0"><?php echo $author_name; ?> escreveu <?php echo $post_count; ?> posts</p>
+               <a href="<?php echo get_author_posts_url($author_id); ?>" class="text-blue-500 block hover:underline">Veja todos os posts de <?php echo $author_name; ?></a>
+            </div>
+         </div>
+      </div>
+
+      <div>
+         <?php
+         $current_post_id = get_the_ID();
+
+         // Obtém as tags do post atual
+         $tags = wp_get_post_tags($current_post_id);
+
+         if ($tags)
+         {
+            $tag_ids = array();
+            foreach ($tags as $tag)
+            {
+               $tag_ids[] = $tag->term_id;
+            }
+
+            $related_posts = get_posts(array(
+               'tag__in' => $tag_ids,
+               'post__not_in' => array($current_post_id),
+               'posts_per_page' => 3,
+               'orderby' => 'rand'
+            ));
+         }
+
+         ?>
+
+      </div>
+   </div>
+
+   <div>
+      <h4 class="py-4 text-xl font-bold text-gray-800">
+         <?php esc_html_e('Posts relacionados', 'portaldoenvelhecimento') ?>
+      </h4>
+      <div class="container p-0 m-0 grid lg:grid-cols-3">
+         <?php if ($related_posts) :
+            foreach ($related_posts as $post) :
+               $thumbnail_url = get_the_post_thumbnail_url($post->ID, 'thumbnail');
+         ?>
+               <div class="col-span-1 flex gap-4 items-center">
+                  <div class="w-1/2">
+                     <a href="<?php echo get_permalink($post->ID) ?>">
+                        <img class="w-full rounded-md object-cover h-[7.5rem]" src="<?php echo $thumbnail_url ?>" class="w-full">
+                     </a>
+                  </div>
+                  <div class="w-1/2">
+                     <a href="<?php echo get_permalink($post->ID) ?>">
+                        <h3 class="text-sm font-medium"><?php echo $post->post_title; ?></h3>
+                     </a>
+                  </div>
+               </div>
+         <?php endforeach;
+         endif; ?>
+      </div>
+   </div>
+
+</article><!-- #post-<?php the_ID(); ?> -->
