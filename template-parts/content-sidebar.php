23b7d2048e381304c0c5f4642223945481daf5ab
Renato Marques <renatodelimamarques@gmail.com>
2023-09-30 17:59:14 -0300

    🚧 Sidebar

--- template-parts/content-sidebar.php
+++ template-parts/content-sidebar.php
@@ -1,8 +1,10 @@
-<div class="w-[17.5rem] bg-[#D9D9D9] rounded-lg p-4 h-fit">
+<div class="w-[17.5rem] bg-[#D9D9D9] rounded-lg p-4 h-fit flex flex-col gap-6">
    <?php //get_sidebar('sidebar-1')
    ?>
    <div>
-      <h5><?php esc_html_e('Você também pode gostar...') ?></h5>
+      <h5 class="mb-6 font-semibold text-lg">
+         <?php esc_html_e('Você também pode gostar...') ?>
+      </h5>
       <?php
       $posts = get_posts(array(
          'numberposts' => 3
@@ -36,7 +38,15 @@
 
    </div>
    <div>
-      <h5><?php esc_html_e('Newsletter semanal', 'portaldoenvelhecimento') ?></h5>
+      <form action="" class="flex flex-col gap-3">
+         <h5 class="font-semibold text-lg">
+            <?php esc_html_e('Newsletter', 'portaldoenvelhecimento') ?>
+         </h5>
+         <p class="m-0 italic text-gray-800 text-sm">Cadastre-se e receba o conteúdo mais atualizado sobre o envelhecimento em seu e-mail!</p>
+         <input name="newsletter-email" type="email" placeholder="Endereço de e-mail" required>
+         <p class="m-0 italic text-gray-500 text-xs">Ao se cadastrar você está concordando com os termos e em receber informações sobre a nossa programação de cursos e lançamentos.</p>
+         <button class="bg-yellow-400 rounded-lg py-2 px-4 uppercase font-medium text-base text-black" type="submit">QUERO ME CADASTRAR!</button>
+      </form>
    </div>
    <div>
       <h5><?php esc_html_e('Categorias', 'portaldoenvelhecimento') ?></h5>
